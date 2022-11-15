7ba8409a2b0ecb5387b316610cd1261e976078c3
chaitanya44444 <93338022+chaitanya44444@users.noreply.github.com>
2022-11-15 16:02:52 +0530

    Create work.py

---
+++ work.py
@@ -0,0 +1,6 @@
+length=int(input("What is the length in meters?"))
+breadth=int(input("What is the breadth in meters?"))
+if breadth==length:
+    print("it is indeed a square")
+else:
+    print("not a square")
